a90a3665b6ecbf727860ccaf090a9e32d09755f5
Nadin Nadin <nadin.ts@gmail.com>
2015-06-04 17:05:59 +0300

    сделала интерактивную карту с указателем

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("mymap", {
+        center: [59.93, 30.32],
+        zoom: 15
+    });
+
+    myPlacemark = new ymaps.Placemark([59.93, 30.32], {
+
+    }, {
+
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map-marker.svg',
+            iconImageSize: [30, 42],
+            iconImageOffset: [-3, -42]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
