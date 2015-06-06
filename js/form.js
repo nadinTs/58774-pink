c62396ad8704ed4bfdf69a017d87ac229c4dad66
Nadin Nadin <nadin.ts@gmail.com>
2015-06-06 17:39:18 +0300

    Форма: отправка формы с помощью ajax

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     var elements = document.querySelectorAll(".input-numeric");
 
@@ -11,10 +11,10 @@
         var minus = parent.querySelector(".input-numeric__minus");
         var plus = parent.querySelector(".input-numeric__plus");
 
-        minus.addEventListener("click", function() {
+        minus.addEventListener("click", function () {
             changeNumber(false);
         });
-        plus.addEventListener("click", function() {
+        plus.addEventListener("click", function () {
             changeNumber(true);
         });
 
@@ -26,11 +26,66 @@
             }
             if (operation) {
                 input.value = value + 1;
-            } else  {
-                if (value  > 0 ) {
+            } else {
+                if (value > 0) {
                     input.value = value - 1;
                 }
             }
         }
     }
 })();
+
+(function () {
+    if (!("FormData" in window)) {
+        return;
+    }
+
+    var form = document.querySelector(".form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        var data = new FormData(form);
+
+        request(data, function(response) {
+           console.log(response);
+        });
+    });
+
+    function request(data, fn) {
+        var xhr = new XMLHttpRequest();
+        var time = (new Date()).getTime();
+
+        xhr.open("post", "http://simonenko.su/academy/echo?" + time);
+
+        xhr.addEventListener("readystatechange", function () {
+            if (xhr.readyState == 4) {
+                fn(xhr.responseText);
+            }
+        });
+        xhr.send(data);
+    }
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
