37cdc719db1a6771ab1f4ac818f5c800fba39cea
Nadin Nadin <nadin.ts@gmail.com>
2015-06-07 12:36:18 +0300

    доработка числовых полей

--- js/form.js
+++ js/form.js
@@ -26,14 +26,70 @@
             }
             if (operation) {
                 input.value = value + 1;
+
             } else {
-                if (value > 0) {
+                if (value > 1) {
                     input.value = value - 1;
                 }
             }
         }
     }
 })();
+
+(function () {
+
+    var elements = document.querySelectorAll(".input-numeric-companion");
+
+    for (var i = 0; i < elements.length; i++) {
+        initNumberField(elements[i]);
+    }
+
+    function initNumberField(parent) {
+        var input = parent.querySelector("input");
+        var minus = parent.querySelector(".input-numeric-companion__minus");
+        var plus = parent.querySelector(".input-numeric-companion__plus");
+        var area = document.querySelector("#companion_list");
+        var template = document.querySelector("#companion-template").innerHTML;
+
+        minus.addEventListener("click", function () {
+            changeNumber(false);
+        });
+        plus.addEventListener("click", function () {
+            changeNumber(true);
+
+        });
+
+        function changeNumber(operation) {
+            var value = Number(input.value);
+
+            if (isNaN(value)) {
+                value = 0;
+            }
+            if (operation) {
+                input.value = value + 1;
+                console.log(input.value);
+
+
+                var html = Mustache.render(template, {
+                    "number": Number(input.value)
+                });
+
+                area.innerHTML = area.innerHTML + html;
+
+
+            } else {
+                if (value > 1) {
+                    input.value = value - 1;
+                    area.removeChild(area.children[area.children.length - 1]);
+                }
+            }
+        }
+    }
+})();
+
+
+
+
 
 (function () {
     if (!("FormData" in window)) {
@@ -47,8 +103,8 @@
 
         var data = new FormData(form);
 
-        request(data, function(response) {
-           console.log(response);
+        request(data, function (response) {
+            console.log(response);
         });
     });
 
@@ -67,6 +123,8 @@
     }
 
 })();
+
+
 
 
 
